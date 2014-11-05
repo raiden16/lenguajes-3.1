5181377e74d1dfbaaa5749fce69e599efaa6f5d2
T107 <T107@PC247>
2014-11-04 21:42:00 -0600

    en insertar viene lo de la tarea

--- src/main/java/com/jc/elementos/model/Insertar.java
+++ src/main/java/com/jc/elementos/model/Insertar.java
@@ -5,9 +5,11 @@
  */
 package com.jc.elementos.model;
 
+import java.util.ArrayList;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -16,12 +18,22 @@ import org.hibernate.Transaction;
 public class Insertar {
     public static void main(String args[]){
     SessionFactory sesion=    NewHibernateUtil.getSessionFactory();
-  Session mala=  sesion.openSession();
-  Transaction t=mala.beginTransaction();
+  Session ses=  sesion.openSession();
+  Transaction t=ses.beginTransaction();
   
-                 mala.save(new Trabajador("juan", "campos", "martinez"));
-             t.commit();
-                 sesion.close();
-                 System.out.println("Se ha guardado este trabajadorcito");
+              //Actualizar: put
+  ses.update(new Trabajador(2,"conejo","blas con","escopeta atras"));
+               // Eliminar: delete
+  ses.delete(new Trabajador(2,"conejo","blas con","escopeta atras"));
+               //Efectuar una busqueda list(): get
+  ArrayList<Trabajador> tra=(ArrayList<Trabajador>) ses.createCriteria(Trabajador.class).list();
+              for(Trabajador tonto:tra){
+                 System.out.println(tonto);
+              }
+              //Buscar por Id: get
+  Trabajador uno= (Trabajador) ses.createCriteria(Trabajador.class).add(Restrictions.idEq(1)).uniqueResult();
+              t.commit();
+              ses.close();
+                  
     }
 }
